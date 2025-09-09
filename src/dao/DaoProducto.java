e9f4043cdf9d62b6a3fd8037d64bbf0d2fbc2969
MarcosParla <marcosparla@hotmail.com>
2025-09-09 20:31:47 -0300

    Packages y Clases agregadas

---
+++ src/dao/DaoProducto.java
@@ -0,0 +1,21 @@
+package dao;
+
+import entidad.Producto;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class DaoProducto {
+
+    private String host   = "jdbc:mysql://localhost:3306/";
+    private String user   = "root";
+    private String pass   = "root";
+    private String dbName = "bdInventario?useUnicode=yes&characterEncoding=UTF-8&useSSL=false";
+
+}
